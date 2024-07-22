9a66e3c86b165a8ac358aefa6e4cc85fa7479d82
fazrilrama <fazrilramadhan2000@gmail.com>
2024-07-22 20:29:15 +0700

    update sorty by distance

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -9,26 +9,38 @@ class JsonController extends Controller
 {
     public function index()
     {
-        $json = Storage::get('data.json');
-        $data = json_decode($json, true);
+        // Load JSON files
+        $json1 = json_decode(file_get_contents(storage_path('app/json-1.json')), true);
+        $json2 = json_decode(file_get_contents(storage_path('app/json-2.json')), true);
 
-        $groupedData = [];
+        $workshops = collect($json2['data'])->keyBy('code');
 
-        foreach ($data as $item) {
-            $warehouseId = $item['warehouse_id'];
-            if (!isset($groupedData[$warehouseId])) {
-                $groupedData[$warehouseId] = [
-                    'warehouse_id' => $warehouseId,
-                    'total_rented_space' => 0,
-                    'items' => []
-                ];
-            }
-            $groupedData[$warehouseId]['total_rented_space'] += $item['rented_space'];
-            $groupedData[$warehouseId]['items'][] = $item;
-        }
+        $combinedData = collect($json1['data'])->map(function ($item) use ($workshops) {
+            $workshop = $workshops->get($item['booking']['workshop']['code']);
 
-        $result = array_values($groupedData);
+            return [
+                'name' => $item['name'],
+                'email' => $item['email'],
+                'booking_number' => $item['booking']['booking_number'],
+                'book_date' => $item['booking']['book_date'],
+                'ahass_code' => $item['booking']['workshop']['code'],
+                'ahass_name' => $item['booking']['workshop']['name'],
+                'ahass_address' => $workshop['address'] ?? '',
+                'ahass_contact' => $workshop['phone_number'] ?? '',
+                'ahass_distance' => $workshop['distance'] ?? 0,
+                'motorcycle_ut_code' => $item['booking']['motorcycle']['ut_code'],
+                'motorcycle' => $item['booking']['motorcycle']['name'],
+            ];
+        });
 
-        return response()->json($result);
+        $sortedData = $combinedData->sortBy('ahass_distance')->values();
+
+        $response = [
+            'status' => 1,
+            'message' => 'Data Successfully Retrieved.',
+            'data' => $sortedData
+        ];
+
+        return response()->json($response);
     }
 }
